e27b986854dd1f2ab69b305a6ac6166193e7244b
cheswiksen <Bartosz1>
2018-10-22 17:49:17 +0200

    testy2

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Flight(models.Model):
+    origin = models.CharField(max_length=64)
+    desrtination = models.CharField(max_length=64)
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id} - {self.origin} to {self.desrtination} for {self.duration}"
