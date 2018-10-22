145de80dff34296fa55e3cb86c5910dcb3d80104
cheswiksen <Bartosz1>
2018-10-22 18:12:15 +0200

    40:00/1:41:14

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Flight
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello world")
+    context = {
+        "flights":Flight.objects.all()
+    }
+    return render(request, "flights/index.html", context)
